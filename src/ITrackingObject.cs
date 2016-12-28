0274dc1f7d86de5df6a55130e5ed36a7613c553d
Edward Meng <edwardmeng@live.cn>
2016-12-28 23:41:37 +0800

    Add tracking interface.

---
+++ src/ITrackingObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Lenoard.Core
+{
+    public enum TrackingState
+    {
+        /// <summary>
+		/// The object has been added, and AcceptChanges has not been called.
+		/// </summary>
+		Added = 0,
+        /// <summary>
+        /// The object was deleted using the Delete method of the MetaObject.
+        /// </summary>
+        Deleted,
+        /// <summary>
+        /// The object has been modified and AcceptChanges has not been called.
+        /// </summary>
+        Modified,
+        /// <summary>
+        /// The object has not changed since AcceptChanges was last called.
+        /// </summary>
+        Unchanged
+    }
+
+    public interface ITrackingObject
+    {
+        void AcceptChanges();
+
+        void RejectChanges();
+
+        TrackingState TrackingState { get; }
+    }
+
+    public static class TrackingObjectExtensions
+    {
+        public static bool IsAdded(this ITrackingObject obj)
+        {
+            if(obj == null)throw new ArgumentNullException(nameof(obj));
+            return obj.TrackingState == TrackingState.Added;
+        }
+
+        public static bool IsDeleted(this ITrackingObject obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            return obj.TrackingState == TrackingState.Deleted;
+        }
+
+        public static bool IsModified(this ITrackingObject obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            return obj.TrackingState == TrackingState.Modified;
+        }
+
+        public static bool IsUnchanged(this ITrackingObject obj)
+        {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
+            return obj.TrackingState == TrackingState.Unchanged;
+        }
+    }
+}
